88245d44406432317da63eaa708315a9fa5397ad
HeltonPojo <heltonpj5@gmail.com>
2024-03-18 18:30:41 -0300

    Funcionalidade de Preseca e FrontEnd Funcionais

--- meteoro-back/server.js
+++ meteoro-back/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql'); 
+const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
@@ -7,19 +7,20 @@ app.use(express.json());
 app.use(cors());
 
 //opt/lampp/lampp/manager-linux-x64.run
+//systemctl status mysql
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database:"meteoro"
+    database: "meteoro"
 })
 
 app.post('/login', (req, res) => {
-    const sql = "SELECT Id, Email, Senha FROM Usuario WHERE Email = ? AND Senha = ?";
+    const sql = "SELECT Id, Email, Senha FROM Usuario WHERE Email = ? AND Senha = ? AND esta_na_sede = 1";
     db.query(sql, [req.body.email, req.body.senha], (err, data) => {
-        if(err) return res.json("Erro no Login");
-        if(data.length > 0){
+        if (err) return res.json("Erro no Login: ", err);
+        if (data.length > 0) {
             return res.json("Login Efetuado")
         } else {
             return res.json("Informações incorretas")
@@ -36,25 +37,25 @@ app.post('/marcar-presenca', (req, res) => {
     const sqlUpdatePresente = "UPDATE Usuario SET esta_na_sede = 1 WHERE Id = ?";
 
     // Consulta para verificar se o usuário existe
-    const sqlSelectUser = "SELECT Id, esta_na_sede FROM Usuario WHERE Email = ? AND Senha = ?";
-    
+    const sqlSelectUser = "SELECT Id, esta_na_sede, horas FROM Usuario WHERE Email = ? AND Senha = ?";
+
     // Executa a consulta para verificar o usuário
     db.query(sqlSelectUser, [email, senha], (err, data) => {
-        if(err) {
-            return res.json("Erro no Login");
+        if (err) {
+            return res.json("Erro no Login: ", err);
         }
-        if(data.length > 0 && data[0].esta_na_sede != 1){
+        if (data.length > 0 && data[0].esta_na_sede != 1) {
             // Se o usuário existe e não está presente, executa a consulta para inserir a presença
-            db.query(sqlUpdatePresente, [data[0].Id], (upErr, upData) =>{
-                if(insertErr) {
-                    return res.json("Erro ao marcar presença");
+            db.query(sqlUpdatePresente, [data[0].Id], (upErr, upData) => {
+                if (upErr) {
+                    return res.json("Erro ao Atualizar Presenca do Usuario: ", upErr);
                 }
             });
             db.query(sqlInsert, [data[0].Id], (insertErr, insertData) => {
-                if(insertErr) {
-                    return res.json("Erro ao marcar presença");
+                if (insertErr) {
+                    return res.json("Erro ao Inserir nova Entrada em Horarios: ", insertErr);
                 }
-                return res.json("Presença marcada com sucesso");
+                return res.json(data);
             });
         } else {
             return res.json("Informações incorretas");
@@ -62,37 +63,79 @@ app.post('/marcar-presenca', (req, res) => {
     });
 });
 
+//Esse metodo precia retornar as horas e realizar as operacoes
 app.post('/marcar-saida', (req, res) => {
     // Dados recebidos do corpo da requisição
     const { Id } = req.body;
 
+
     // Consulta para inserir um novo registro na tabela 'Presenca'
-    const sqlInsert = "UPDATE Horarios SET Saida = NOW() WHERE Saida = NULL and IdUsuario = ?";
-    const sqlUpdatePresente = "UPDATE Usuario SET esta_na_sede = 0 WHERE Id = ?";
+    const sqlUpdateSaida = "UPDATE Horarios SET Saida = NOW() WHERE isnull(Saida) AND IdUsuario = ?";
+    const sqlUpdatePresente = "UPDATE Usuario SET esta_na_sede = 0 , horas  = ADDTIME(horas, ?) WHERE Id = ?";
 
     // Consulta para verificar se o usuário existe
     const sqlSelectUser = "SELECT Id, esta_na_sede FROM Usuario WHERE Id = ?";
-    
+    const sqlSelectHora = "SELECT Id, Entrada FROM Horarios WHERE isnull(Saida) AND IdUsuario = ?";
+    const sqlSelectHoraSaida = "SELECT Saida FROM Horarios WHERE Id = ?";
+    var entrada;
+    var saida;
     // Executa a consulta para verificar o usuário
-    db.query(sqlSelectUser, [email, senha], (err, data) => {
-        if(err) {
-            return res.json("Erro no Login");
+    db.query(sqlSelectUser, [Id], (err, data) => {
+        if (err) {
+            return res.json("Erro no Login: ", err);
         }
-        if(data.length > 0 && data[0].esta_na_sede != 0){
+        if (data.length > 0 && data[0].esta_na_sede != 0) {
             // Se o usuário existe e está presente, executa a consulta para inserir a presença
-            db.query(sqlUpdatePresente, [data[0].Id], (upErr, upData) =>{
-                if(insertErr) {
-                    return res.json("Erro ao marcar presença");
+
+            db.query(sqlSelectHora, [data[0].Id], (slHErr, slHora) => {
+                if (slHErr) {
+                    return res.json("Erro no Select Horario: ", slHErr);
                 }
-            });
-            db.query(sqlInsert, [data[0].Id], (insertErr, insertData) => {
-                if(insertErr) {
-                    return res.json("Erro ao marcar presença");
-                }
-                return res.json("Presença marcada com sucesso");
+                entrada = slHora[0].Entrada;
+
+                db.query(sqlUpdateSaida, [data[0].Id], (upSErr, upSData) => {
+                    if (upSErr) {
+                        return res.json("Erro no Update Saida: ", upSErr);
+                    }
+                    db.query(sqlSelectHoraSaida, [slHora[0].Id], (slHSErr, slHSaida) => {
+                        if (slHSErr) {
+                            return res.json("Erro na consulta do Select Saida Horarios: ", slHSErr);
+                        }
+
+                        saida = slHSaida[0].Saida;
+
+                        var diffMillis = saida - entrada;
+                        var diffHoras = diffMillis / (1000 * 60 * 60);
+                        var horas = Math.floor(diffHoras);
+                        var minutos = Math.floor((diffHoras - horas) * 60);
+                        var segundos = Math.floor(((diffHoras - horas) * 60 - minutos) * 60);
+
+                        var horaFormatada = `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+                        db.query(sqlUpdatePresente, [horaFormatada, data[0].Id], (upErr, upData) => {
+                            if (upErr) {
+                                return res.json("Erro no Update Presenca no Usuario: ", upErr);
+                            }
+                            return res.json("Saida marcada com sucesso");
+                        });
+                    });
+                });
             });
         } else {
             return res.json("Informações incorretas");
+        }
+    });
+});
+
+app.get('/membros-presentes', (req, res) => {
+//adicionar nome no Banco de dados
+    const sql = "SELECT Usuario.Id as Id, Email, Cargo, horas, Entrada FROM Usuario INNER JOIN Horarios on Usuario.Id=Horarios.IdUsuario WHERE esta_na_sede = 1 AND isnull(Horarios.Saida)";
+    db.query(sql, (err, data) => {
+        if (err) return res.json("Erro no Consulta Membros Presentes: ", err);
+        if (data.length > 0) {
+            //console.log("Presentes: ", data);
+            return res.json(data);
+        } else {
+            return res.json("")
         }
     });
 });
--- meteoro-front/src/Presenca.js
+++ meteoro-front/src/Presenca.js
@@ -8,20 +8,20 @@ const { Header, Sider } = Layout;
 
 function Presenca() {
     const [userData, setUserData] = useState([]);
-
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
     useEffect(() => {
         fetchData();
     }, []);
 
     const fetchData = async () => {
         try {
-            const response = await axios.get('http://localhost:8081/mebros-presentes');
+            const response = await axios.get('http://localhost:8081/membros-presentes');
             setUserData(response.data);
         } catch (error) {
             console.error('Erro ao buscar dados da API:', error);
         }
     };
-
     function handelSubmit(event) {
         event.preventDefault();
         axios.post('http://localhost:8081/marcar-presenca', { email, senha })
@@ -34,6 +34,7 @@ function Presenca() {
 
     function handleSair(event) {
         event.preventDefault();
+        const Id = event.currentTarget.dataset.userId;
         axios.post('http://localhost:8081/marcar-saida', {Id}).then(res => {
             console.log(res);
             location.reload();
@@ -41,6 +42,7 @@ function Presenca() {
         .catch(err => console.log(err));
     }
 
+    if(userData.length >0 ){
     return (
         <Layout>
             <Sider>
@@ -63,16 +65,43 @@ function Presenca() {
             <div>
                 <h1>Informações do Usuário</h1>
                 {userData.map((user) => (
-                    <div>
-                    <UserCard key={user.id} user={user} />
-                    <button onClick={handleSair}> Sair </button>
-                    </div>
+                    <div key={user.Id}>
+                        <h4>{user.Id}</h4>
+                        <h4>{user.Email}</h4>
+                        <h4>{user.Cargo}</h4>
+                        <h4>{user.horas}</h4>
+                        <h4>{user.Entrada}</h4>
+                        <button onClick={handleSair} data-user-id={user.Id}> Sair </button>
+                    </div> 
                 ))}
-                
             </div>
         </Layout>
-
-    )
+    );}else{
+        return (
+            <Layout>
+                <Sider>
+                    <div className='d-flex vh-100 justify-content-center align-items-center bg-secondary'>
+    
+                        <form onSubmit={handelSubmit}>
+                            <div className='mb-3'>
+                                <label htmlFor="email">Email</label>
+                                <input type="email" placeholder='Email' className='form-control' onChange={e => setEmail(e.target.value)}></input>
+                            </div>
+                            <div className='mb-3'>
+                                <label htmlFor="senha">Senha</label>
+                                <input type="password" placeholder='Senha' className='form-control' onChange={e => setSenha(e.target.value)}></input>
+                            </div>
+                            <button className='btn btn-success'>Entrar</button>
+                        </form>
+    
+                    </div>
+                </Sider>
+                <div>
+                    <h1>Informações do Usuário</h1>                    
+                </div>
+            </Layout>
+        );
+    }
 }
 
 export default Presenca;
